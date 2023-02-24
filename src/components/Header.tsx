95e93ffe8c37cf8a6e373009d8d0219a76216e58
pedroygor <pedroygor2010@hotmail.com>
2023-02-23 23:05:35 -0300

    feat: Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/images/Logo.svg'
+
+export const Header = () => {
+  return (
+    <header className="flex items-center justify-around py-8">
+      <img src={logo} alt="" />
+      <div className="flex items-center justify-center gap-3">
+        <div className="flex h-9 items-center justify-center gap-1 rounded-md bg-produto-purpleLight px-2 text-sm leading-[18px] text-produto-purple">
+          <MapPin size={22} weight="fill" />
+          <span>Banabuiú, CE</span>
+        </div>
+        <Link
+          to="/carrinho"
+          className="flex h-9 w-9 items-center justify-center rounded-md bg-produto-yellowLight"
+        >
+          <ShoppingCart size={22} weight="fill" color="#C47F17" />
+        </Link>
+      </div>
+    </header>
+  )
+}
